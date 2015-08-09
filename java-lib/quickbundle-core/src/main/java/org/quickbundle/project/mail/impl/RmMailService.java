f1f184cd5ea863f541e982c0c189592e9c035656
longxibo@21cn.com <longsebo@21cn.com>
2015-08-09 11:47:28 +0800

    测试提交

--- java-lib/quickbundle-core/src/main/java/org/quickbundle/project/mail/impl/RmMailService.java
+++ java-lib/quickbundle-core/src/main/java/org/quickbundle/project/mail/impl/RmMailService.java
@@ -9,9 +9,13 @@ public class RmMailService implements IRmMailService {
 	private String sendMailUser;
 	private String sendMailPassword;
 	private String mailFrom;
-	
+	//是否使用ssl add by longsebo 2015-04-21  
+	private String ssl;  
+	//增加smtp端口属性 add by longsebo 2015-04-21  
+	private String port;  
+
 	public String getMailSmtpHost() {
-		return mailSmtpHost;
+		return mailSmtpHost; 	
 	}
 	public void setMailSmtpHost(String mailSmtpHost) {
 		this.mailSmtpHost = mailSmtpHost;
@@ -45,12 +49,24 @@ public class RmMailService implements IRmMailService {
 	 * @param aAffix 附件数组{{"显示名", "附件"}, {"a", "/a.jpg"}}
 	 */
 	public void send(String mailto, String subject, String bodyText, String bodyHtml, String[][] aAffix) {
-		RmMailHandler mh = new RmMailHandler(new String[]{mailSmtpHost, sendMailUser, sendMailPassword, mailFrom});
+		RmMailHandler mh = new RmMailHandler(new String[]{mailSmtpHost, sendMailUser, sendMailPassword, mailFrom,port,ssl});
 		mh.send(mailto, subject, bodyText, bodyHtml, aAffix);
 	}
 	
 	public static void main0(String[] args) {
 		RmMailHandler mh = new RmMailHandler(new String[]{"mail.163.com","qb_test","quickbundle","qb_test@163.com"});
 		mh.send("test@quickbundle.org", RmDateHelper.getSysDateTime(), "rt", null, null);
+	}
+	public String getSsl() {
+		return ssl;
+	}
+	public void setSsl(String ssl) {
+		this.ssl = ssl;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
 	}
 }
